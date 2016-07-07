1ccf1e49665c022b39d0427cc0e6cc94740a44dc
Alex Pantelides <alex.pantelides@gmail.com>
2016-07-06 19:28:12 -0700

    Implement getElementsByClassName using recursion
    This function returns an array of elements which contain the class name input into the function.
    
    Using element.childNodes, which gives an array of childeren, we can chech if the class name is contained in the element class list and if it is we push the current element to the array. If the element contains children we loop through each child and check if the class name is contained within its class list and push it to the array if it is. We also check if there are any childrent of the children array and so on and so forth. document.body is the main parent of the page where we start the recursive search function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) { //function with argument for a class name
   // your code here
+  var elements = [] //array which will store each element match
+  function searchNodes(element){ //function searchNodes which will take in an element as an argument
+  	
+  	if(element.classList) { //if the element contains a class check the following
+  		if(element.classList.contains(className)){ //if the element contains the className in its classList
+  			elements.push(element) //push that element to the elements array
+  		}
+  	} 
+  	if(element.childNodes){ //if the element has child elements do the following
+  		var childeren = element.childNodes //syntax symplification
+  		for(var i = 0; i<childeren.length; i++) { //for each childNode of current element
+  			searchNodes(childeren[i]); //call the function for each child
+  		}
+  	}
+  }
+
+  searchNodes(document.body); //document.body is parent to all the elements on the page so we start with it
+  return elements; //return array with all the element matches
 };
